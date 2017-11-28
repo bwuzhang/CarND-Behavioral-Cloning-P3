050d6ec910d2828bdde2d1f7f59cd09d80ad4753
bwuzhang <bwuzhang@gmail.com>
2017-11-27 20:32:27 -0500

    Added new model and video

--- net.py
+++ net.py
@@ -22,12 +22,15 @@ for line in lines:
         current_path = 'sample_data/IMG/' + filename
         image = cv2.imread(current_path)
         image_w_lines = find_lane(image)
-        images.append(image_w_lines)
         measurement = float(line[3])
         if i == 1:
             measurement += 0.25
         elif i == 2:
             measurement -= 0.25
+        if np.absolute(measurement) < 0.1:
+            if np.random.uniform() < 0.5:
+                continue
+        images.append(image_w_lines)
         measurements.append(measurement)
 
 augemented_images, augemented_measurements = [], []
@@ -35,10 +38,12 @@ for image, measurement in zip(images, measurements):
     augemented_images.append(cv2.cvtColor(image, cv2.COLOR_BGR2YUV))
     augemented_measurements.append(measurement)
 
+    # Flip
     image_flipped = cv2.flip(image, 1)
     augemented_images.append(cv2.cvtColor(image_flipped, cv2.COLOR_BGR2YUV))
     augemented_measurements.append(measurement * -1.0)
 
+    # Random brightness
     image_hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
     image_hsv = np.array(image_hsv, dtype = np.float64)
     random_bright = .5+np.random.uniform()
@@ -50,6 +55,30 @@ for image, measurement in zip(images, measurements):
     augemented_images.append(image_brightness)
     augemented_measurements.append(measurement)
 
+    # Random shadow
+    image_shadow = np.array(image, dtype = np.float64)
+    h,w = image_shadow.shape[0:2]
+    mid = np.random.randint(0,w)
+    factor = np.random.uniform(0.6,0.8)
+    if np.random.rand() > .5:
+        image_shadow[:,0:mid,:] *= factor
+    else:
+        image_shadow[:,mid:w,:] *= factor
+    image_shadow = np.array(image_shadow, dtype = np.uint8)
+    augemented_images.append(cv2.cvtColor(image_shadow, cv2.COLOR_BGR2YUV))
+    augemented_measurements.append(measurement)
+
+    # Random shift
+    image_shift = image
+    h,w,_ = image_shift.shape
+    horizon = 2*h/5
+    v_shift = np.random.randint(-h/8,h/8)
+    pts1 = np.float32([[0,horizon],[w,horizon],[0,h],[w,h]])
+    pts2 = np.float32([[0,horizon+v_shift],[w,horizon+v_shift],[0,h],[w,h]])
+    M = cv2.getPerspectiveTransform(pts1,pts2)
+    image_shift = cv2.warpPerspective(image_shift,M,(w,h), borderMode=cv2.BORDER_REPLICATE)
+    augemented_images.append(cv2.cvtColor(image_shift, cv2.COLOR_BGR2YUV))
+    augemented_measurements.append(measurement)
 
 X_train = np.array(augemented_images)
 y_train = np.array(augemented_measurements)
@@ -59,26 +88,27 @@ from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Dropout
 from keras.layers import Conv2D, MaxPooling2D, Cropping2D
 from keras.optimizers import Adam
+from keras import regularizers
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((70,25),(0,0))))
 
-model.add(Conv2D(24, kernel_size=(5,5), strides=(2, 2), activation='relu'))
-model.add(Conv2D(36, kernel_size=(5,5), strides=(2, 2), activation='relu'))
-model.add(Conv2D(48, kernel_size=(5,5), strides=(2, 2), activation='relu'))
-model.add(Conv2D(64, kernel_size=(3,3), activation='relu', padding='same'))
-model.add(Conv2D(64, kernel_size=(3,3), activation='relu'))
+model.add(Conv2D(24, kernel_size=(5,5), strides=(2, 2), activation='elu', kernel_regularizer=regularizers.l2(0.01)))
+model.add(Conv2D(36, kernel_size=(5,5), strides=(2, 2), activation='elu', kernel_regularizer=regularizers.l2(0.01)))
+model.add(Conv2D(48, kernel_size=(5,5), strides=(2, 2), activation='elu', kernel_regularizer=regularizers.l2(0.01)))
+model.add(Conv2D(64, kernel_size=(3,3), activation='elu', padding='same', kernel_regularizer=regularizers.l2(0.01)))
+model.add(Conv2D(64, kernel_size=(3,3), activation='elu', kernel_regularizer=regularizers.l2(0.01)))
 model.add(Flatten())
-model.add(Dense(100, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(50, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(10, activation='relu'))
+model.add(Dense(100, activation='elu', kernel_regularizer=regularizers.l2(0.01)))
+# model.add(Dropout(0.5))
+model.add(Dense(50, activation='elu', kernel_regularizer=regularizers.l2(0.01)))
+# model.add(Dropout(0.5))
+model.add(Dense(10, activation='elu', kernel_regularizer=regularizers.l2(0.01)))
 model.add(Dense(1))
 
 adam = Adam(lr = 0.0001)
 model.compile(loss='mse', optimizer=adam)
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=4)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=10)
 
 model.save('model.h5')
